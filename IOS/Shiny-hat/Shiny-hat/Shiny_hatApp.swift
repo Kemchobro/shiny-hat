51b108cb513a002d2b6a47296f2d2705a11b60dc
Vidhatu Patel <vidhatu@gmail.com>
2023-04-09 23:22:27 -0400

    added swift :(

---
+++ IOS/Shiny-hat/Shiny-hat/Shiny_hatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shiny_hatApp.swift
+//  Shiny-hat
+//
+//  Created by Vidhatu Patel on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Shiny_hatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
